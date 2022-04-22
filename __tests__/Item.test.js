238524590f8357d29d229b0b5dd3818face39860
unknown <seth.keating@gmail.com>
2022-04-22 03:32:05 -0400

    unignored tests; Villager inventory and Item further function and tests

---
+++ __tests__/Item.test.js
@@ -0,0 +1,68 @@
+const Item = require('../lib/Item');
+const luxon = require('../js/luxon.min.js');
+
+
+// Has name, value
+test ('creates an item object', () => {
+  let item = new Item({ name: 'cart', value: 800 });
+
+  expect(item.name).toBe('cart');
+  expect(item.value).toEqual(800);
+  // expect(Number(item.value)).toBe(800);
+});
+
+// Comparison of separate equivalent items
+test ('test generic items are equivalent', () => {
+  let itemOne = new Item({ name: 'coin', value: 100 });
+  let itemTwo = new Item({ name: 'coin', value: 100 });
+  let itemThree = new Item({ name: 'candle', value: 100 });
+
+  expect(itemOne.equals(itemTwo)).toBeTruthy();
+  expect(itemTwo.equals(itemOne)).toBeTruthy();
+  expect(itemOne.equals(itemThree)).toBeFalsy();
+  expect(itemTwo.equals(itemThree)).toBeFalsy();
+});
+
+// Comparison of separate unique items
+test ('test unique items are or are not equivalent', () => {
+  let coin = new Item({ id: 'tc', unique: false, name: 'coin', value: 100 });
+  let fakeCoin = new Item({ id: 'fc', unique: true, name: 'coin', value: 100 });
+  let forgedCoin = new Item({ id: 'fc', unique: true, name: 'coin', value: 100 });
+
+  expect(coin.equals(coin)).toBeTruthy();
+
+  // Cannot match a common item with a unique item
+  expect(coin.equals(fakeCoin)).toBeFalsy();
+  expect(coin.equals(forgedCoin)).toBeFalsy();
+  
+  expect(fakeCoin.equals(forgedCoin)).toBeTruthy();
+  expect(forgedCoin.equals(fakeCoin)).toBeTruthy();
+})
+
+// Find indices for equivalent items in an array
+test ('finds items in array by equivalency', () => {
+  let coin = new Item({ name: 'coin', value: 100 });
+  let candle = new Item({ name: 'candle', value: 100 });
+  let pocket = [];
+  let inventory = [new Item({ name: 'coin', value: 100 })];
+
+  expect(coin.foundWithin(pocket)).toBeFalsy();
+
+  expect(coin.foundWithin(inventory)).toEqual([0]);
+  expect(candle.foundWithin(inventory)).toEqual([]);
+
+  inventory.push(coin,candle);
+  expect(coin.foundWithin(inventory)).toEqual([0,1]);
+  expect(candle.foundWithin(inventory)).toEqual([2]);
+  
+  let fakeCoin = new Item({ id: 'fc', unique: true, name: 'coin', value: 100 });
+  let forgedCoin = new Item({ id: 'fc', unique: true, name: 'coin', value: 100 });
+
+  pocket.push(fakeCoin);
+  // Finding unique items
+  expect(fakeCoin.foundWithin(pocket)).toEqual([0]);
+  pocket.push(forgedCoin);
+  expect(fakeCoin.foundWithin(pocket)).toEqual([0,1]);
+  
+});
+
---
+++ __tests__/Villager.test.js
@@ -0,0 +1,134 @@
+const Villager = require('../lib/Villager');
+const Item = require('../lib/Item');
+const luxon = require('../js/luxon.min.js');
+
+
+// Has name, creation date
+test ('creates a villager object', () => {
+  let villager = new Villager({ name: 'Alain' });
+
+  expect(villager.name).toBe('Alain');
+  expect(luxon.DateTime.fromISO(villager.hatched).isValid).toBeTruthy();
+
+  villager = new Villager({});
+  expect(villager.name).toBe('Noisome');
+});
+
+// Has hunger level, food consumption
+test ('has hunger level, eats food at 8, recovers hunger', () => {
+  const villager = new Villager({ name: 'Antonio', hunger: 8 });
+
+  expect(villager.hunger).toBe(8);
+  expect(villager.hungry_at).toBe(8);
+  expect(villager.eats).toBe(6);
+
+  expect(villager.hungry()).toBeTruthy();
+  expect(villager.hungry(9)).toBeTruthy();
+  expect(villager.hungry(5)).toBeFalsy();
+
+  let regained = villager.eat(1);
+  expect(villager.hunger).toBe(7);
+  expect(regained).toBe(1);
+
+  expect(villager.hungry()).toBeFalsy();
+
+  regained = villager.eat();
+  expect(villager.hunger).toBe(1);
+  expect(regained).toBe(6);
+
+  regained = villager.eat();
+  expect(villager.hunger).toBe(0);
+  expect(regained).toBe(1);
+});
+
+
+// Has wealth(, can spend it?)
+test ('has wealth, can afford items & can spend money', () => {
+  const villager = new Villager({ name: 'Galford' });
+  const cart = { name: 'cart', price: 800 };
+  const coin = { name: 'coin', price: 100 };
+
+  expect(villager.wealth).toBe(0);
+  expect(villager.afford(cart)).toBeFalsy();
+  expect(villager.afford(coin)).toBeFalsy();
+  expect(villager.afford(800)).toBeFalsy();
+  expect(villager.spend(cart.price)).toBeFalsy();
+  expect(villager.spend(coin.price)).toBeFalsy();
+
+  let wealth = villager.earn(1200);
+
+  expect(wealth).toBe(1200);
+  expect(villager.wealth).toBe(1200);
+  expect(villager.afford(cart)).toBeTruthy();
+  expect(villager.afford(coin)).toBeTruthy();
+  expect(villager.afford(800)).toBeTruthy();
+
+  wealth = villager.spend(cart.price);
+  
+  expect(wealth).toBe(400);
+  expect(villager.wealth).toBe(400);
+  expect(villager.afford(cart)).toBeFalsy();
+  expect(villager.afford(coin)).toBeTruthy();
+  expect(villager.spend(cart.price)).toBeFalsy();
+});
+
+// Has inventory, can gain items
+test ('has an inventory, can receive items', () => {
+  const galford = new Villager({ name: 'Galford' });
+  const coin = new Item({ name: 'coin', value: 100 });
+  const key = new Item({ name: 'key', value: 100 });
+  const candle = new Item({ name: 'candle', value: 100 });
+
+  expect(galford.inventory).toEqual([]);
+  expect(galford.holds(coin)).toBeFalsy();
+
+  const aragon = new Villager({ name: 'Aragon', inventory: [coin, key] });
+  expect(aragon.inventory).toEqual([coin, key]);
+  expect(aragon.holds(coin)).toBeTruthy();
+  expect(coin.foundWithin(aragon.inventory)).toEqual([0]);
+  expect(aragon.holds(key)).toBeTruthy();
+  expect(key.foundWithin(aragon.inventory)).toEqual([1]);
+  expect(aragon.holds(candle)).toBeFalsy();
+
+  aragon.gain(candle);
+  expect(candle.foundWithin(aragon.inventory)).toEqual([2]);
+  expect(aragon.holds(candle)).toBeTruthy();
+});
+// Can lose items
+test ('can lose items', () => {
+  const coin = new Item({ name: 'coin', value: 100 });
+  const key = new Item({ name: 'key', value: 100 });
+  const candle = new Item({ name: 'candle', value: 100 });
+  const galford = new Villager({ name: 'Galford',
+    inventory: [coin, coin, key] });
+
+  // Does not carry item
+  expect(galford.drop(candle)).toBeFalsy();
+
+  // Drops only like item
+  expect(galford.holds(key)).toBeTruthy();
+  expect(galford.drop(key)).toBeTruthy();
+  expect(galford.holds(key)).toBeFalsy();
+  
+  // Drops one of several like items
+  expect(galford.holds(coin)).toBeTruthy();
+  expect(galford.drop(coin)).toBeTruthy();
+  expect(galford.holds(coin)).toBeTruthy();
+
+  expect(galford.drop(coin)).toBeTruthy();
+  expect(galford.holds(coin)).toBeFalsy();
+})
+
+// Has experience level (tied to profession action?)
+
+
+// Has profession, can access its values
+
+
+// Has profession and current action
+
+
+// Has profession, can complete current
+
+
+// Has profession, can form a priority action
