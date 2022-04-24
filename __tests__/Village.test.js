9f4d56edc3b655c74ce3f79cadb806a584f82d0c
abel-baker <seth.keating@gmail.com>
2022-04-23 23:53:13 -0400

    further Village tests

--- __tests__/Village.test.js
+++ __tests__/Village.test.js
@@ -3,7 +3,7 @@ const Villager = require('../lib/Villager');
 const Farmer = require('../lib/professions/Farmer');
 const luxon = require('../js/luxon.min.js');
 
-
+// Has name, creation date
 test ('creates a village object', () => {
   let village = new Village({ name: 'Zarma' });
 
@@ -14,14 +14,34 @@ test ('creates a village object', () => {
   expect(village.name).toBe('Sibon');
 });
 
+// Wealth is earned by resource transactions (sales) and reflects overall
+// financial prosperity of the village.  May be used to purchase construction
+test ('has wealth, earns and spends', () => {
+  let village = new Village({ wealth: 80 });
+
+  expect(village.wealth).toBe(80);
+
+  village.earn(100);
+  expect(village.wealth).toBe(180);
+
+  // Return false if unable to make transaction
+  expect(village.spend(200)).toBeFalsy();
+});
+
 // Resources include basic food, basic materials, crafted materials and
 // components, crafted novelties and luxuries, composite items 
 test ('has basic resources, receives and spends them', () => {
   let sibon = new Village();
 
   expect(sibon.resources['food']).toBe(0);
-  expect(sibon.receive({ 'food': 20 })).toBe(20);
-  expect(sibon.reduce({'food': 5 })).toBe(15);
+  expect(sibon.receive({ 'food': 20 })).toBe({ 'food': 20 });
+  expect(sibon.reduce({ 'food': 5 })).toBe({ 'food': 15 });
+
+  expect(sibon.receive({ 'wood': 10, 'stone': 12 }));
+  expect(sibon.resources['wood']).toBe(10);
+  expect(sibon.count('wood')).toBe(10);
+  expect(sibon.resources['stone']).toBe(12);
+  expect(sibon.count('stone')).toBe(12);
 });
 
 // Experiences weather
